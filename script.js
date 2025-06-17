53fbb6de7051ba2cfbf00ec32b10ab9fe69ca81c
bodaspedroesalete <bodaspedroesalete@gmail.com>
2025-06-17 09:58:07 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+document.getElementById("rsvpForm").addEventListener("submit", async function(e) {
+  e.preventDefault();
+  const formData = new FormData(this);
+  await fetch("https://script.google.com/macros/s/AKfycbzQ80lrIPB3wYGpL4mW-ZfMHbW025KEBCCzgVue_9yOrjDZSZW_r2HLU4eASuaV7yse/exec", {
+    method: "POST",
+    body: formData,
+  });
+  alert("Confirmação recebida com sucesso!");
+  this.reset();
+});
